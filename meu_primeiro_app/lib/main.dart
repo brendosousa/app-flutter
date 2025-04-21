79a304d2b16a3b5d010cacd2598bba72b85f3f64
Brendo Sousa <brendo.n.s.sousa@gmail.com>
2025-04-20 22:42:15 -0300

    Adicionando widgets

--- meu_primeiro_app/lib/main.dart
+++ meu_primeiro_app/lib/main.dart
@@ -115,8 +115,23 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ],
+            Container(
+                constraints: BoxConstraints.expand(
+                  height: Theme.of(context).textTheme.headlineMedium!.fontSize! * 1.1 + 200.0,
+                ),
+                padding: const EdgeInsets.all(8.0),
+                color: Colors.blue[600],
+                alignment: Alignment.center,
+                transform: Matrix4.rotationZ(0.1),
+                child: Text('Hello World',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineMedium!
+                      .copyWith(color: Colors.white)),
+            )
+          ]
         ),
+
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -136,11 +151,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      /*floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),*/
     );   // This trailing comma makes auto-formatting nicer for build methods.
       
   }
