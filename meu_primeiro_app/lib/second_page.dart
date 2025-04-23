f59f3285b3ff6a8f6b8b20e197f95f069e2a822a
Brendo Sousa <brendo.n.s.sousa@gmail.com>
2025-04-22 22:26:43 -0300

    Add botão de retorno à pagina inicial - com alguma Exception

--- meu_primeiro_app/lib/second_page.dart
+++ meu_primeiro_app/lib/second_page.dart
@@ -9,17 +9,59 @@ class MinhaSegundaPagina extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      /*home: const MyHomePage(title: 'Minha página faz qualquer coisa'),*/
+
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First Flutter App'),
         ),
         body: Center(
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
-          ),
+          child: ElevatedButton(
+              child: Text("Voltar pra página inicial"),
+              onPressed: () {
+                Navigator.pop(context);
+              } 
+            )
         ),
       ),
     );
   }
 }
+
+/*class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text("Voltar pra página inicial"),
+              onPressed: () {
+                Navigator.pop(context);
+              } 
+            )
+
+          ]
+        )
+      ),
+    );  
+  }
+
+
+}*/
